95fa7a345a99d899667b4947e98552e8f76ab3f9
Casper Nielsen <casperkmn@gmail.com>
2022-11-30 08:39:24 +0100

    removed a service uses another service

--- OAI-API/Services/QuestionService.cs
+++ OAI-API/Services/QuestionService.cs
@@ -6,21 +6,18 @@ namespace OAI_API.Services
     public class QuestionService : IQuestionService
     {
         private readonly IQuestionRepository _questionRepository;
-        private readonly IAnswerService _answerService;
 
         public QuestionService(
-            IQuestionRepository questionRepository, 
-            IAnswerService answerService)
+            IQuestionRepository questionRepository)
         {
             _questionRepository = questionRepository;
-            _answerService = answerService;
         }
 
         public async Task<Question> GetQuestionAsync(int questionId)
         {
             var questionDTO = await _questionRepository.GetQuestionAsync(questionId);
 
-            var answer = await _answerService.GetAnswerAsync(questionDTO.AnswerId);
+            var answer = new BaseAnswer() { AnswerId = questionDTO.AnswerId };
 
             return new Question(questionDTO, answer);
         }
@@ -29,7 +26,7 @@ namespace OAI_API.Services
         {
             var questionDTO = await _questionRepository.RegisterQuestionAsync(new QuestionDTO(question));
 
-            var answer = await _answerService.GetAnswerAsync(questionDTO.AnswerId);
+            var answer = new BaseAnswer() { AnswerId = questionDTO.AnswerId };
 
             return new Question(questionDTO, answer);
         }
